a24bef581482e9595baa33f736b487dcbac7ec1a
Sergey Pashin <S.E.Pashin@yandex.ru>
2022-05-30 13:04:01 +0300

    Убраны функции переработанные/закомментированные

--- src/presenter/presenter-main.js
+++ src/presenter/presenter-main.js
@@ -81,9 +81,7 @@ export default class PresenterMain {
     const getAddEventListenerToPointEdit = () => { /*Ф добавляющая слушатель для кнопки редактирования которая в свою очередь при нажатии изменит отображение с точки на форму для данной точки.*/
       const pointBtn = pointComponent.element.querySelector('.event__rollup-btn');
       pointBtn.addEventListener('click', pointFormEditOpen);
-    }
-
-
+    };
 
     getAddEventListenerToPointEdit(); /*Запуск функции. TODO обратить внимание что слушатель устанавливается на все точки */
     render(pointComponent, this.#TripList.element);
