e90f35b0b22a24193a8dd432c9a8abbde86bb6a7
Sergey Pashin <S.E.Pashin@yandex.ru>
2022-06-01 20:06:11 +0300

    Добавлено отображение сообщения при нулевом количестве точек

--- src/presenter/presenter-main.js
+++ src/presenter/presenter-main.js
@@ -4,11 +4,15 @@ import TripListView from '../view/trip-list-view.js';
 import CreateAddNewPointView from '../view/add-new-point-view.js';
 import CreateItemPointView from '../view/item-point-view.js';
 import CreateEditPointView from '../view/edit-point-view';
+import CreateMessageZeroPoint from "../view/zero-point-message-view/zero-point-message-view";
+
+const NUMBER_POINT_TO_SHOW_MESSAGE_ZERO_POINT = 0;
 
 export default class PresenterMain {
   #SortBlock = new SortView();
   #TripList = new TripListView();
   #AddNewPoint = new CreateAddNewPointView();
+  #massageZeroPoint = new CreateMessageZeroPoint();
   #mainContainer = null;
   #pointsModel = null;
   #points = [];
@@ -17,8 +21,15 @@ export default class PresenterMain {
     this.#mainContainer = mainContainer; /*куда передаю*/
     this.#pointsModel = pointsModel;
     this.#points = [...this.#pointsModel.points]; /*TODO ...this это оператор расширения из ES 6, он позволяет вставить массив в другой массив как бы между элементами массива. В данном случае он позволяет создать новый массив "развернув" старый и изменять его без изменений "старого" массива.  */ /*Вызываю метод get(не как функцию так как синтаксис) который возвращает результат вычисляемого приватного свойства которое в свою очередь является созданным массивом*/
-    render(this.#SortBlock, this.#mainContainer); /*В рендер передается (Обязательно с new) сначала что отрисовать а потом куда отрисовать это.*/
-    render(this.#TripList, this.#mainContainer);/*Добавляю элемент в главный контейнер*/
+
+
+
+    if (this.#points.length === NUMBER_POINT_TO_SHOW_MESSAGE_ZERO_POINT) {
+      render(this.#massageZeroPoint, this.#mainContainer);/*Добавляю элемент в главный контейнер*/
+    } else {
+      render(this.#SortBlock, this.#mainContainer); /*В рендер передается (Обязательно с new) сначала что отрисовать а потом куда отрисовать это.*/
+      render(this.#TripList, this.#mainContainer);/*Добавляю элемент в главный контейнер*/
+    }
 
     for (let i = 0; i < this.#points.length; i++) {
       this.#renderPointToTripList(this.#points[i]); /*Вызов функции рендера на каждой точке из массива.*/
@@ -26,6 +37,8 @@ export default class PresenterMain {
   };
 
   #renderPointToTripList = (point) => { /*Функция, создает компонент с разметкой на основании данных и отрисовывает/рендерит его в перечень точек маршрутов, приватная, может быть вызвана только в данном классе.*/
+
+
     const pointComponent = new CreateItemPointView(point);
     const pointEditComponent = new CreateEditPointView(point);
 
---
+++ src/view/zero-point-message-view/zero-point-message-view.js
@@ -0,0 +1,21 @@
+import {createElement} from '../../render.js';
+import {createZeroPointMessageTemplate} from "./zero-point-message-view-template";
+
+export default class CreateMessageZeroPoint {
+  #element = null;
+
+  get template() { /*Метод класса. Метод что бы получить шаблон разметки. Все методы кроме конструктора придумываются самостоятельно.*/
+    return createZeroPointMessageTemplate(); /**/
+  }
+
+  get element() { /*Метод класса. Метод позволяет на основе шаблона создать DOM элемент. Импортирует что то из render.js*/
+    if (!this.#element) { /*Вольная интерпретация сингл-тон или по другому условие при котором не будут делаться дубли. Не совсем понятно. todo Как это работает? */
+      this.#element = createElement(this.template); /*Функция создающая по шаблону элемент. Вызываю функцию, передаю в нее шаблонную строчку с html а она возвращает DOM элемент todo возвращает или создает на странице? Необходимо проверить.. Находится в render.js*/
+    }
+    return this.#element;
+  }
+
+  removeElement() { /*Метод класса. Позволяет удалить этот элемент. Созданный ? TODO ?*/
+    this.#element = null; /**/
+  }
+}
